89f512767c8e0a38fa2230e1fa415294a3ef4bd7
Silvanos Eric <ericoderito@gmail.com>
2024-07-15 20:33:22 +0300

    feat: enhance timer with manual increment and decrement controls
    - Rename counter variable to `counterEl` for clarity
    - Add DOM references for minus and plus buttons to manage timer manually
    - Refactor timer update logic into `updateCounterEl` function for better code reuse
    - Attach event listeners to plus and minus buttons to increment and decrement the timer value on
      user interaction

--- js/challenge.js
+++ js/challenge.js
@@ -3,7 +3,9 @@ let timer = 0;
 let timerId = null;
 
 // Elements on interest
-const counter = document.querySelector("h1#counter");
+const counterEl = document.querySelector("h1#counter");
+const minusButtonEl = document.querySelector("button#minus");
+const plusButtonEl = document.querySelector("button#plus");
 
 // State update functions
 function incrementTimer() {
@@ -24,10 +26,25 @@ function updateElTextContent(el, newTextContent) {
   el.textContent = newTextContent;
 }
 
-// As soon as the page is loaded the timer should increment every second
+function updateCounterEl() {
+  updateElTextContent(counterEl, timer);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+  // As soon as the page is loaded the timer should increment every second
   repeatAfterInterval(() => {
     incrementTimer();
-    updateElTextContent(counter, timer);
+    updateCounterEl();
   }, 1000);
+
+  // Manually increment and decrement the counter using plus and minus buttons
+  minusButtonEl.addEventListener("click", () => {
+    decrementTimer();
+    updateCounterEl();
+  });
+
+  plusButtonEl.addEventListener("click", () => {
+    incrementTimer();
+    updateElTextContent(counterEl, timer);
+  });
 });
