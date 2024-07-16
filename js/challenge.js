7ef8e28dd05807a5237394895bbb71cb656bdff7
Silvanos Eric <ericoderito@gmail.com>
2024-07-16 04:49:31 +0300

    refactor(js): update variable names and improve like functionality
    - Rename timer-related variables to count for better readability.
    - Add functionality to track and display the number of likes for each count.
    - Adjust functions to handle count and likes correctly.
    - Update DOM manipulation to support new features.

--- js/challenge.js
+++ js/challenge.js
@@ -1,19 +1,42 @@
 // Application state
-let timer = 0;
+let count = 0;
 let timerId = null;
+let numberOfLikes = 0;
+let lastCheckedCount = -1;
+let likesForPreviousCount = 0;
 
-// Elements on interest
+// Elements of interest
 const counterEl = document.querySelector("h1#counter");
-const minusButtonEl = document.querySelector("button#minus");
-const plusButtonEl = document.querySelector("button#plus");
+const minusBtnEl = document.querySelector("button#minus");
+const plusBtnEl = document.querySelector("button#plus");
+const heartBtnEl = document.querySelector("button#heart");
+const likesEl = document.querySelector("ul.likes");
 
 // State update functions
-function incrementTimer() {
-  timer++;
+function incrementCount() {
+  count++;
 }
 
-function decrementTimer() {
-  timer--;
+function decrementCount() {
+  count--;
+}
+
+function incrementLikes() {
+  numberOfLikes++;
+}
+
+function resetLikes() {
+  numberOfLikes = 0;
+}
+
+function incrementLikesForCurrentNumber() {
+  if (!hasTimerIncreased()) {
+    incrementLikes();
+  } else {
+    likesForPreviousCount = numberOfLikes;
+    resetLikes();
+    incrementLikes();
+  }
 }
 
 // Utility functions
@@ -21,30 +44,64 @@ function repeatAfterInterval(cb, interval) {
   timerId = setInterval(cb, interval);
 }
 
-// Update an element's text content
 function updateElTextContent(el, newTextContent) {
   el.textContent = newTextContent;
 }
 
-function updateCounterEl() {
-  updateElTextContent(counterEl, timer);
+function updateCounterElTextContent() {
+  updateElTextContent(counterEl, count);
 }
 
+function hasTimerIncreased() {
+  if (count > lastCheckedCount) {
+    lastCheckedCount = count;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function addCurrentCountLikes() {
+  const currentCount = count;
+
+  // Check if element already exist in the DOM,
+  // if it exists, update it, else create it
+  const element = document.getElementById(`${count}`);
+
+  if (element) {
+    element.textContent = `${count} has been liked ${numberOfLikes} times`;
+  } else {
+    const likeEl = document.createElement("li");
+    likeEl.setAttribute("id", `${count}`);
+    likeEl.textContent = `${count} has been liked ${numberOfLikes} ${
+      numberOfLikes === 1 ? "time" : "times"
+    }`;
+    likesEl.appendChild(likeEl);
+  }
+}
+
+// DOM Manipulation
 document.addEventListener("DOMContentLoaded", () => {
   // As soon as the page is loaded the timer should increment every second
   repeatAfterInterval(() => {
-    incrementTimer();
-    updateCounterEl();
+    incrementCount();
+    updateCounterElTextContent();
   }, 1000);
 
   // Manually increment and decrement the counter using plus and minus buttons
-  minusButtonEl.addEventListener("click", () => {
-    decrementTimer();
-    updateCounterEl();
+  minusBtnEl.addEventListener("click", () => {
+    decrementCount();
+    updateCounterElTextContent();
   });
 
-  plusButtonEl.addEventListener("click", () => {
-    incrementTimer();
-    updateElTextContent(counterEl, timer);
+  plusBtnEl.addEventListener("click", () => {
+    incrementCount();
+    updateElTextContent(counterEl, count);
+  });
+
+  // Display the number of likes associated with with a number
+  heartBtnEl.addEventListener("click", () => {
+    incrementLikesForCurrentNumber();
+    addCurrentCountLikes();
   });
 });
